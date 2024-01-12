2940d6bdbe30a157b3775edad6a114fef2d934e5
Enel Villafranca <enel.villafranca@tigears.com>
2024-01-12 10:12:06 -0400

    ajuste imagenes users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,17 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::create(['name' => 'Fletes RGC', 'email' => 'admin@admin.com', 'password' => bcrypt('Project2023_'), 'username' => 'Admin', 'status' => 0, 'shw_password' => 'Project2023_', 'email_verified_at' => '2023-10-16 17:54:25']);
+        $admin = User::create([
+            'role' => 1,
+            'logo' =>'user-1.png',
+            'logo_top' =>'logo-top.png',
+            'logo_top_sm' =>'logo-sm.png',
+            'name' => 'Fletes RGC',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('Project2023_'),
+            'username' => 'Admin',
+            'status' => 0,
+            'shw_password' => 'Project2023_',
+            'email_verified_at' => '2023-10-16 17:54:25']);
     }
 }
