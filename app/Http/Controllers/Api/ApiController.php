853ff454ba797d0d2f6a5d5058c1cf33cf0706a0
Kiibo-Groups <kiibogroups@gmail.com>
2025-05-31 12:34:49 -0600

    mejora de logica de funcion getGSMInfo

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,7 +1,9 @@
-<?php namespace App\Http\Controllers\api;
+<?php
+
+namespace App\Http\Controllers\api;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller; 
+use App\Http\Controllers\Controller;
 use App\Providers\SocketServer;
 use App\Http\Controllers\{BlacsolController, SamsaraController};
 use Illuminate\Support\Facades\Log;
@@ -12,9 +14,9 @@ use App\Services\PacketParserService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Tymon\JWTAuth\Contracts\JWTSubject; 
-use DB;  
-use Redirect; 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use DB;
+use Redirect;
 use App\Models\{User, GpsDevices, vehicle_units, Getgsminfo, Rutas};
 
 
@@ -23,12 +25,13 @@ use App\Events\RuptelaServer;
 use Pusher\Pusher;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 use Maatwebsite\Excel\Facades\Excel;
-class ApiController  extends Controller  
+
+class ApiController  extends Controller
 {
-    public function __construct()
+	public function __construct()
 	{
-		$this->middleware('authApi:api',['except' => [
-			'getToken',  
+		$this->middleware('authApi:api', ['except' => [
+			'getToken',
 			'PosiCont',
 			'Idle',
 			'Mov',
@@ -46,25 +49,24 @@ class ApiController  extends Controller
 		]]);
 	}
 
-    public function welcome()
+	public function welcome()
 	{
 		return response()->json(['data' => "Bienvenido al API de RGC"]);
 	}
 
 	public function getToken(Request $request)
-    {
-        try {
-            $token = new User;
+	{
+		try {
+			$token = new User;
 			return response()->json($token->GenToken($request));
-
-        } catch (\Exception $th) {
-			return response()->json(['status' => 'ERROR','code' => 500, 'message' => $th->getMessage()], 500);
+		} catch (\Exception $th) {
+			return response()->json(['status' => 'ERROR', 'code' => 500, 'message' => $th->getMessage()], 500);
 		}
-    }
+	}
 
-    public function Webhook_rgc_api(Request $request)
-    {
-        try {
+	public function Webhook_rgc_api(Request $request)
+	{
+		try {
 			$validator = Validator::make($request->all(), [
 				'unidad'        => 'required|string|min:15',
 				'destino'       => 'required|string|min:100',
@@ -86,14 +88,14 @@ class ApiController  extends Controller
 				]);
 			}
 
-            $data = $request->all();
-			
+			$data = $request->all();
+
 			// return response()->json($data, 200);
 			return response()->json([
 				'message' => "informacion_recibida",
 				'status' => "success",
 				"code" => 200
-			],200);
+			], 200);
 		} catch (\Exception $th) {
 			return response()->json([
 				'data' => [],
@@ -102,22 +104,20 @@ class ApiController  extends Controller
 				"code" => 500
 			]);
 		}
-    }
+	}
 
 	public function webhook_rgc_csv(Request $request)
 	{
 		try {
 			$data = $request->all();
 			if (isset($data['file'])) {
-				
+
 				$array = Excel::toArray(new Rutas, $data['file']);
 				$i = 0;
-				$input = []; 
-				foreach($array[0] as $a)
-				{
-					$i++; 
-					if($i > 1)
-					{
+				$input = [];
+				foreach ($array[0] as $a) {
+					$i++;
+					if ($i > 1) {
 						$input['unidad'] 		= $a[0];
 						$input['destino'] 		= $a[1];
 						$input['origen'] 		= $a[2];
@@ -136,9 +136,8 @@ class ApiController  extends Controller
 					'message' => "informacion_recibida",
 					'status' => "success",
 					"code" => 200
-				],200);
-				
-			}else {
+				], 200);
+			} else {
 				return response()->json([
 					'data' => $request->all(),
 					'message' => "file_not_found",
@@ -182,66 +181,61 @@ class ApiController  extends Controller
 		 */
 
 		$paqueteHex = $request->input('packet');
-		Log::info('[*]['.date('H:i:s')."] Paquete Hexadecimal Recibido: ".json_encode($paqueteHex));
+		if (empty($paqueteHex)) {
+			return response()->json([
+				'status' => 400,
+				'message' => 'Packet vacío o no recibido.'
+			], 400);
+		}
+
 		$parser = new PacketParserService(json_encode($paqueteHex));
 		$datos = $parser->parse();
 
-		Log::info('[*]['.date('H:i:s')."] Data Decifrada: ".json_encode($datos));
-		$data = $datos; 
-		$gsminfo = new Getgsminfo;
+		Log::info('[*][' . date('H:i:s') . "] Data Decifrada: " . json_encode($datos));
 
-		// Validamos el IMEI si existe
-		if (isset($data['imei'])) {
-			$chkImei = Getgsminfo::where('imei', $data['imei'])->first();
-			if (isset($chkImei->id)) {
-				$chkImei->packet = $paqueteHex;
-				$chkImei->imei = $data['imei'];
-				$chkImei->longitude = $data['longitude'];
-				$chkImei->latitude = $data['latitude'];
-				$chkImei->altitude = $data['altitude'];
-				$chkImei->angle = $data['angle'];
-				$chkImei->speed = $data['speed'];
-				$chkImei->date_update = now();
-
-				$chkImei->save();
-				
-			}else {
-				// Validamos el Dispositivo GPS
-				$chkGPS = GpsDevices::where('uuid_device', $data['imei'])->first();
-				if (isset($chkGPS) && $chkGPS->id) {
-					$data['gps_devices_id'] = $chkGPS->id;
-				}
-
-				// Validamos el Vehiculo
-				$chkVehicle = vehicle_units::where('gps_devices_id', $data['gps_devices_id'])->first();
-				if (isset($chkVehicle) && $chkVehicle->id) {
-					$data['vehicle_units_id'] = $chkVehicle->id;
-				}
-
-				// $chkVehicle = vehicle_units::whereHas('getGPS', function($q) use($data) {
-				// 	$q->where('uuid_device', $data['imei']);
-				// })->first();
-
-				// if (isset($chkVehicle) && $chkVehicle->id) {
-				// 	$data['vehicle_units_id'] = $chkVehicle->id;
-				// }
-
-				// Creamos
-				$data['date_update'] = now();
-				$data['packet'] = $paqueteHex;
-				$gsminfo->create($data);
-			}
-
-			return response()->json([
-				'status' => 200,
-				'message' => "data_receibed"
-			],200);
-		}else {
+		// Verificar que tenga IMEI
+		if (empty($datos['imei'])) {
 			return response()->json([
 				'status' => 400,
-				'message' => "IMEI_not_found"
-			],400);
+				'message' => 'IMEI_not_found'
+			], 400);
 		}
+
+		$imei = $datos['imei'];
+		$datos['packet'] = $paqueteHex;
+		$datos['date_update'] = now();
+
+		$registroExistente = Getgsminfo::where('imei', $imei)->first();
+		if ($registroExistente) {
+			$registroExistente->fill([
+				'packet'    => $datos['packet'],
+				'longitude' => $datos['longitude'] ?? null,
+				'latitude'  => $datos['latitude'] ?? null,
+				'altitude'  => $datos['altitude'] ?? null,
+				'angle'     => $datos['angle'] ?? null,
+				'speed'     => $datos['speed'] ?? null,
+				'date_update' => $datos['date_update'],
+			])->save();
+		} else {
+			// Buscar dispositivo GPS
+			$gps = GpsDevices::where('uuid_device', $imei)->first();
+			if ($gps) {
+				$datos['gps_devices_id'] = $gps->id;
+
+				// Buscar vehículo asociado al dispositivo
+				$vehiculo = vehicle_units::where('gps_devices_id', $gps->id)->first();
+				if ($vehiculo) {
+					$datos['vehicle_units_id'] = $vehiculo->id;
+				}
+			}
+
+			Getgsminfo::create($datos);
+		}
+
+		return response()->json([
+			'status' => 200,
+			'message' => 'data_received'
+		]);
 	}
 
 	public function setPusher()
@@ -263,7 +257,7 @@ class ApiController  extends Controller
 	public function getAllDispositives()
 	{
 
-		$getAll = Getgsminfo::with('getGPS','getVehicle')->get([
+		$getAll = Getgsminfo::with('getGPS', 'getVehicle')->get([
 			'id',
 			'longitude',
 			'latitude',
@@ -290,14 +284,14 @@ class ApiController  extends Controller
 	 */
 	public function PosiCont()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de PosiCont.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de PosiCont.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->PosiCont();
 			$req  = [];
 
-			for ($i=0; $i < count($data); $i++) { 
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -309,13 +303,13 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->PosiCont();
 				unset($BlacSol);
-			} 
+			}
 
 			return response()->json([
-				'data'   => $req, 
+				'data'   => $req,
 				'status' => true,
 				'code'   => 200
 			]);
@@ -330,14 +324,14 @@ class ApiController  extends Controller
 
 	public function Idle()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de Idle.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de Idle.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->Idle();
 			$req = [];
-			
-			for ($i=0; $i < count($data); $i++) { 
+
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -349,10 +343,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->Idle();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -367,19 +361,18 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function Mov()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de Mov.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de Mov.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->Mov();
 			$req = [];
-			
-			for ($i=0; $i < count($data); $i++) { 
+
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -391,10 +384,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->Mov();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -409,19 +402,18 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function Detenido()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de Detenido.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de Detenido.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->Detenido();
 			$req = [];
-			
-			for ($i=0; $i < count($data); $i++) { 
+
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -433,10 +425,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->Detenido();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -451,12 +443,11 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function ChkIgn()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de IgnEnc.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de IgnEnc.... .\r\n");
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->ChkIgn();
@@ -472,12 +463,11 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function IgnApa()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de IgnApa.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de IgnApa.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
@@ -493,7 +483,7 @@ class ApiController  extends Controller
 				'dateTimeUTC' => '',
 			]);
 			// $req  = [];
-			
+
 			// for ($i=0; $i < count($data); $i++) { 
 			// 	$BlacSol = new BlacsolController(
 			// 		$data[$i]['username'],	// username
@@ -506,7 +496,7 @@ class ApiController  extends Controller
 			// 		$data[$i]['odometer'],	// odometer
 			// 		$data[$i]['dateTimeUTC']	// dateTimeUTC
 			// 	);
-				
+
 			// 	$req[] = $BlacSol->IgnApa();
 			// 	unset($BlacSol);
 			// } 
@@ -524,19 +514,18 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function DetJamm()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de DetJamm.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de DetJamm.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->GetAllVehicleAssignments();
 			$req = [];
 
-			for ($i=0; $i < count($data); $i++) { 
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -548,10 +537,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->DetJamm();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -566,19 +555,18 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function DesconBat()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de DesconBat.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de DesconBat.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->GetAllVehicleAssignments();
 			$req = [];
 
-			for ($i=0; $i < count($data); $i++) { 
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -590,10 +578,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->DesconBat();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -608,18 +596,17 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 
 	public function ReconBat()
 	{
-		Log::channel()->info('[*]['.date('H:i:s')."] Inicializacion de Peticion de ReconBat.... .\r\n");
+		Log::channel()->info('[*][' . date('H:i:s') . "] Inicializacion de Peticion de ReconBat.... .\r\n");
 
 		try {
 			$Samsara = new SamsaraController;
 			$data = $Samsara->GetAllVehicleAssignments();
 			$req = [];
-			for ($i=0; $i < count($data); $i++) { 
+			for ($i = 0; $i < count($data); $i++) {
 				$BlacSol = new BlacsolController(
 					$data[$i]['username'],	// username
 					$data[$i]['imei'],	// imei
@@ -631,10 +618,10 @@ class ApiController  extends Controller
 					$data[$i]['odometer'],	// odometer
 					$data[$i]['dateTimeUTC']	// dateTimeUTC
 				);
-				
+
 				$req[] = $BlacSol->ReconBat();
 				unset($BlacSol);
-			} 
+			}
 
 
 			return response()->json([
@@ -649,6 +636,5 @@ class ApiController  extends Controller
 				'error'  => $th->getMessage()
 			]);
 		}
-
 	}
 }
